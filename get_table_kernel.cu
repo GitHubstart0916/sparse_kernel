230aaa83793a93166267cd6a15514f8e08a21561
ciel <chthollyboss@qq.com>
2025-11-21 06:52:14 +0000

    [chore] modify comments in

--- get_table_kernel.cu
+++ get_table_kernel.cu
@@ -1,27 +1,30 @@
 #include <ATen/cuda/CUDAContext.h>
 #include <torch/extension.h>
 
-constexpr int kTokenNum = 8192;
-constexpr int kBs = 1;
-constexpr int kSeqlenQMax = 8192;
+constexpr int WARP_SIZE = 32;
+constexpr int THREADS_PER_BLOCK = 64;
+constexpr int WARPS_PER_BLOCK = THREADS_PER_BLOCK / WARP_SIZE;
+
 constexpr int kHeadGroup = 2;
 constexpr int kSparseBlockSize = 64;
 constexpr int kSparseTopK = 96;
-const int THREADS_PER_BLOCK = 64;
 
-// topk_idx: [head_group, token_num, kSparseTopK]: int32 [2, 8192, 96]
-// block_table: [batch_size, seqlen_q_max]: int32 [1, 8192]
-// token_to_bs: [token_num]: int32  [8192]
-// token_pos_in_bs: [token_num]: int32 [8192]
-// seqlen_q: [batch_size]: int32    [1]
-// out_block_table: [token_num, head_group, kSparseTopK * kSparseBlockSize]:
-// int32 [2, 8192, 96 * 64] seqlen_q_max: int
+/// @param topk_idx: [head_group, token_num, kSparseTopK] topk sparse block index
+/// @param block_table: [batch_size, seqlen_q_max / page_size] page table mapping token idx to page idx
+/// @param token_to_bs: [token_num] mapping from token idx to batch idx
+/// @param token_pos_in_bs: [token_num] mapping from token idx to position in batch
+/// @param seqlen_q: [batch_size] sequence length for each batch
+/// @param out_block_table: [token_num, head_group, kSparseTopK * kSparseBlockSize]
+/// @param token_num
+/// @param seqlen_q_max
+/// @param page_size
 __global__ void get_block_table_cuda(const int *topk_idx, const int *block_table, const int *token_to_bs,
                                      const int *token_pos_in_bs, const int *seqlen_q, int *out_block_table,
                                      const int seqlen_q_max, const int token_num, const int page_size = 1) {
+
   auto tidx = threadIdx.x;
-  auto widx = tidx / 32;
-  auto lane_id = tidx % 32;
+  auto widx = tidx / WARP_SIZE;
+  auto lane_id = tidx % WARP_SIZE;
 
   auto token_idx = blockIdx.x;
   auto bs = token_to_bs[token_idx];
@@ -30,12 +33,11 @@ __global__ void get_block_table_cuda(const int *topk_idx, const int *block_table
   auto num_pages = kSparseBlockSize / page_size;
 
   for (int h = 0; h < kHeadGroup; h++) {
-    for (int k = widx; k < kSparseTopK; k += (THREADS_PER_BLOCK / 32)) {
-      // each warp process one topk
+    for (int k = widx; k < kSparseTopK; k += WARPS_PER_BLOCK) {
       int sp_bidx = topk_idx[h * token_num * kSparseTopK + token_idx * kSparseTopK + k];
       if (sp_bidx < 0)
         continue;
-      for (int i = lane_id; i < num_pages; i += 32) {
+      for (int i = lane_id; i < num_pages; i += WARP_SIZE) {
         int token_idx_in_batch = sp_bidx * kSparseBlockSize + i * page_size;
         auto token_valid = (token_idx_in_batch < seq_len) && (token_idx_in_batch < pos_in_bs);
         auto out_block_idx =
@@ -72,18 +74,14 @@ torch::Tensor get_block_table_wrapper(const torch::Tensor &topk_idx,        // [
                                                )
                                       .contiguous();
 
-  const int NUM_BLOCKS = token_num;
-
-  // 5. 调用 CUDA kernel
-  get_block_table_cuda<<<NUM_BLOCKS, THREADS_PER_BLOCK>>>(topk_idx.data_ptr<int>(), block_table.data_ptr<int>(),
-                                                          token_to_bs.data_ptr<int>(), token_pos_in_bs.data_ptr<int>(),
-                                                          seqlen_q.data_ptr<int>(), out_block_table.data_ptr<int>(),
-                                                          seqlen_q_max, token_num);
+  get_block_table_cuda<<<token_num, THREADS_PER_BLOCK>>>(topk_idx.data_ptr<int>(), block_table.data_ptr<int>(),
+                                                         token_to_bs.data_ptr<int>(), token_pos_in_bs.data_ptr<int>(),
+                                                         seqlen_q.data_ptr<int>(), out_block_table.data_ptr<int>(),
+                                                         seqlen_q_max, token_num);
 
   return out_block_table;
 }
 
-// --- 4. PyTorch 扩展模块注册 ---
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("get_block_table", &get_block_table_wrapper, "Sparse Attention Block Table Getter (CUDA)");
 }
